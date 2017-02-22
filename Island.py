2a7b386d149c404f508b3caf9c025393939abe7d
EstebanLopez <esteban.lopez3@upr.edu>
2017-02-22 18:15:42 -0400

    Island mission mplementation

--- Island.py
+++ Island.py
@@ -1,8 +1,45 @@
 # Island Mision
-
-
+import Movement
+import Line
 def start():
     print()
+    inFront = Movement.listen()
+    aligned = False
+    while not aligned:
+        #move
+        inFront = Movement.listen()
+         if inFront == ?:
+            aligned = True
+    Movement.forward(10)
+    alignedTop = False
+
+    while not alignedTop
+	if ():
+		Movement.move_right()
+
+
+    elif ():
+		Movement.move_left()
+
+         elif ():
+		Movement.move_back()
+
+	elif ():
+		Movement.move_front()
+
+	else
+		alignedTop = True
+
+surface(100%)
+
+if (pressure == 0)
+	surface(0)
+
+
+return 0
+
+
+
 
 
 """
