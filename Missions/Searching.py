35a9089a6e036e889407549e88ba409931278f50
FelixGonzalez <felix.gonzalez3@upr.edu>
2017-03-15 18:44:57 -0500

    Adding Searching to git

---
+++ Missions/Searching.py
@@ -0,0 +1,22 @@
+from Missions import Align
+from Missions import Movement
+def start():
+
+    """
+    object = 0;
+    buoy()
+def buoy():
+    TakePicture()
+    if(detectObjet()):
+        object = object + 1
+        Align.start(10,10)
+        if(object > 1):
+            if(getDistance() < 10) :
+                touchBuoy()
+                return 0;
+        else :
+            Movement.forward(2)
+           return buoy();
+    return 0;
+
+    """
